d45f16e921d692ff42d401e95a47efad76c11a8e
Sohail Shaukat Selod <sohail47k@gmail.com>
2019-10-16 03:29:20 +0530

    Code formatted

--- Day 0 JD/code.py
+++ Day 0 JD/code.py
@@ -1,18 +1,19 @@
 #! python3
 
-grid = [[0,0,0,0,0],
-        [1,0,1,1,1],
-        [1,0,1,0,1],
-        [0,1,1,1,0],
-        [1,0,1,1,0]]
+grid = [[0, 0, 0, 0, 0],
+        [1, 0, 1, 1, 1],
+        [1, 0, 1, 0, 1],
+        [0, 1, 1, 1, 0],
+        [1, 0, 1, 1, 0]]
 
 
 class Path:
     def __init__(self, anchor, direction, length):
-        self.anchor = anchor                                #(i,j)
-        self.direction = direction                          # True for Up , False for Left
+        self.anchor = anchor  # (i,j)
+        # True for Up , False for Left
+        self.direction = direction
         self.length = length
-    
+
     def maximumPathFinder(grid, anchor):
         path_up = Path.upPathCalculator(grid, anchor)
         path_left = Path.leftPathCalculator(grid, anchor)
@@ -20,9 +21,9 @@ class Path:
             return path_up
         else:
             return path_left
-        
+
     def upPathCalculator(grid, anchor):
-        i,j = anchor
+        i, j = anchor
         length = 0
         while grid[i][j] == 1 and i > -1:
             length += 1
@@ -33,10 +34,9 @@ class Path:
             except:
                 break
         return Path(anchor, True, length)
-            
-    
+
     def leftPathCalculator(grid, anchor):
-        i,j = anchor
+        i, j = anchor
         length = 0
         while grid[i][j] == 1 and j > -1:
             length += 1
@@ -47,10 +47,10 @@ class Path:
             except:
                 break
         return Path(anchor, False, length)
-    
+
     def pathPainter(self, grid):
-        grid = [ [ 'W' for el in ( grid[0] ) ] for row in grid ]
-        i,j = self.anchor
+        grid = [['W' for el in (grid[0])] for row in grid]
+        i, j = self.anchor
         length = self.length
         if self.direction:
             while length:
@@ -63,13 +63,14 @@ class Path:
                 j -= 1
                 length -= 1
         return grid
-    
+
+
 paths = []
 for i, row in enumerate(grid):
     for j, spot in enumerate(row):
         if spot == 1:
-            path = Path.maximumPathFinder(grid = grid, anchor = (i,j) )
-            paths.append(path) 
+            path = Path.maximumPathFinder(grid=grid, anchor=(i, j))
+            paths.append(path)
 maximum_runway_length = 0
 maximum_runway_path = None
 for path in paths:
@@ -77,4 +78,4 @@ for path in paths:
         maximum_runway_length = path.length
         maximum_runway_path = path
 grid = maximum_runway_path.pathPainter(grid)
-print(*grid, sep = '\n')
+print(*grid, sep='\n')

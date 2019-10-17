6b11fc69b78871c3aa6255a3a885fa35592b494d
Sohail Shaukat Selod <sohail47k@gmail.com>
2019-10-18 02:42:14 +0530

    Day 10 Apple JobScheduler added

--- Day 10/Apple function/code.py
+++ Day 10/Apple function/code.py
@@ -3,7 +3,7 @@ import time
 
 
 def jobScheduler(func, period):
-    for _ in range(10):
+    for _ in range(10):                 #this could be just while True: but i didn't want to fill up your cli
         func()
         time.sleep(period/1000)
 

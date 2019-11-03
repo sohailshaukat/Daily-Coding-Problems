0a7928704dd501d217b2768b3ec2cfccbc05296e
Sohail Shaukat Selod <sohail47k@gmail.com>
2019-11-04 01:33:47 +0530

    Day 37 Google Powerset Solution added

---
+++ Day 37/Google Power Set/code.py
@@ -0,0 +1,17 @@
+#! python3
+import random
+
+arr = [1, 2, 3]
+
+stack = [{}]
+ptr = 0
+while ptr < len(stack):
+    parent = stack[ptr]
+    for elem in arr:
+        temp = list(parent)
+        temp.append(elem)
+        if set(temp) not in stack:
+            stack.append(set(temp))
+    ptr += 1
+
+print(stack)

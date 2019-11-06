3ac3b19f7d28aa77ceeb0d9f5ac7b920b68a28ff
Sohail Shaukat Selod <sohail47k@gmail.com>
2019-11-07 04:04:20 +0530

    Formatting

--- Day 41/Facebook Itinerary/code.py
+++ Day 41/Facebook Itinerary/code.py
@@ -1,25 +1,25 @@
 #!python 3
 
 
-def isHeadPresent(head, flights):
+def is_head_present(head, flights):
     for ptr, flight in enumerate(flights):
         if flight[0] == head:
-            return (True, ptr)
-    return (False, -1)
+            return True, ptr
+    return False, -1
 
 
 # flights = [('SFO', 'COM'), ('COM', 'YYZ')]
 flights = [('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')]
 # head = 'COM'
 head = 'YUL'
-trigger = isHeadPresent(head, flights)
+trigger = is_head_present(head, flights)
 halts = [head]
 while trigger[0]:
     loc = trigger[1]
     head = flights[loc][1]
     halts.append(head)
     del flights[loc]
-    trigger = isHeadPresent(head, flights)
+    trigger = is_head_present(head, flights)
 if len(flights) == 0:
     print(halts)
 else:
